b591f34526254b4d29087716c17ef180fa1efa1c
Igor Duca <yelldutz@gmail.com>
2026-04-27 10:35:45 -0300

    Initial Commit

---
+++ Prompty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Prompty
+//
+//  Created by Igor Duca on 27/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Prompty/PromptyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromptyApp.swift
+//  Prompty
+//
+//  Created by Igor Duca on 27/04/26.
+//
+
+import SwiftUI
+
+@main
+struct PromptyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
